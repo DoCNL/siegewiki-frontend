26022797dada909a0ab9506b66efc9b35d8f2b52
Sjoerd van der Bok <sabok@avans.nl>
2018-12-12 06:30:10 +0100

    heroku test

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
+// const express = require('express');
+// const app = express();
+
+// app.listen(process.env.PORT || 3000, () => {
+//     console.log('App is ready for requests.')
+//   })
+//   .on('error', (error) => {
+//     console.warn('Warning', error.toString());
+// });
+
+// module.exports = app;
+
 const express = require('express');
+const http = require('http');
+const path = require('path');
+
 const app = express();
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log('App is ready for requests.')
-  })
-  .on('error', (error) => {
-    console.warn('Warning', error.toString());
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-module.exports = app;
+const port = process.env.PORT || '9000';
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log(`Running`));
